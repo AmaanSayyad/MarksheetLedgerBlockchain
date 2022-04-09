d7b82d8830593f757dc330e5add9132795b91022
JigarJoshi <jigar.pj@somaiya.edu>
2022-04-09 19:25:08 +0530

    add: contract connection added

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  contractConnection();
+  var contract = contractConnection();
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
